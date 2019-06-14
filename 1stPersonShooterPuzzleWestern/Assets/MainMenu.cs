11e026030d3b9e443a346584733c41f3b8b358ae
gxn30 <gxneomies@gmail.com>
2019-06-14 12:26:09 +0200

    herkansingTest1

---
+++ 1stPersonShooterPuzzleWestern/Assets/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("lol");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ 1stPersonShooterPuzzleWestern/Assets/eindMuisTest.cs
@@ -0,0 +1,43 @@
+﻿/*
+  probeersels: 
+  //EndScreen.SetActive(true);
+            //endScreen.SetActive(true); bij scorescript
+   public gameobject en .setactive
+   verschillende menus en buttonlayouts
+   autoreset
+ */
+using UnityEngine;
+
+public class eindMuisTest : MonoBehaviour
+{
+    public float currentTime = 0f;
+    public float startingTime = 10f;
+    
+
+    private void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        print (currentTime);
+
+        if (currentTime < 0)
+            {
+            // public void Quit()
+            { Debug.Log("rq");
+                Application.Quit();
+            }
+        }
+
+
+
+    }
+
+
+
+
+
+}
